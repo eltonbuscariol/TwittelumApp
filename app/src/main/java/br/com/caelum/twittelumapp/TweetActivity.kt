e8fc4e3a5d7a1bb50874369cbdf33e2c7bee0c74
eltonbuscariol <elton.buscariol@gmail.com>
2019-01-21 20:24:56 -0200

    init

---
+++ app/src/main/java/br/com/caelum/twittelumapp/TweetActivity.kt
@@ -0,0 +1,12 @@
+package br.com.caelum.twittelumapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TweetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tweet)
+    }
+}
