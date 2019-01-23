d3d078f060fe6e28c8f9eb2163035b7f16243370
eltonbuscariol <elton.buscariol@gmail.com>
2019-01-22 22:32:36 -0200

    init

--- app/src/main/java/br/com/caelum/twittelumapp/ListaTweetsActivity.kt
+++ app/src/main/java/br/com/caelum/twittelumapp/ListaTweetsActivity.kt
@@ -1,6 +1,8 @@
 package br.com.caelum.twittelumapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_lista_tweets.*
@@ -15,6 +17,11 @@ class ListaTweetsActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tweets)
 
         listaTweets.adapter = arrayAdapter
+
+        fabAddTweet.setOnClickListener {
+            val intent = Intent(this, TweetActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/br/com/caelum/twittelumapp/TweetActivity.kt
+++ app/src/main/java/br/com/caelum/twittelumapp/TweetActivity.kt
@@ -2,10 +2,13 @@ package br.com.caelum.twittelumapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import br.com.caelum.twittelumapp.modelo.Tweet
 
 class TweetActivity : AppCompatActivity() {
 
@@ -13,18 +16,34 @@ class TweetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweet)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
-        val btnPublicar = findViewById<Button>(R.id.btnPublicar)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tweet_menu, menu)
+        return true
+    }
 
-        btnPublicar.setOnClickListener {
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when(item?.itemId) {
+        R.id.salvar_tweet_menu -> {
             publicaTweet()
+            finish()
+            true
         }
+        android.R.id.home -> {
+            finish()
+            true
+        }
+        else -> false
     }
 
     private fun publicaTweet() {
 
         val conteudo = findViewById<EditText>(R.id.tweet_mensagem)
+        val tweet = Tweet(conteudo.text.toString())
 
-        Toast.makeText(this, conteudo.text, Toast.LENGTH_SHORT).show()
+        // Qualquer das duas maneiras
+        //Toast.makeText(this, tweet.toString(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$tweet", Toast.LENGTH_SHORT).show()
     }
 }
